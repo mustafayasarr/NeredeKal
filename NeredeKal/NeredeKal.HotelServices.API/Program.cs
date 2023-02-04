dc35ac9b746c30407593e7a2f2fc93b423bee4a8
Mustafa YAŞAR <mustafa@mustafayasar.info>
2023-02-04 23:31:47 +0300

    add docker-compose

--- NeredeKal/NeredeKal.HotelServices.API/Program.cs
+++ NeredeKal/NeredeKal.HotelServices.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using NeredeKal.HotelServices.API.Bootstrapper;
+using NeredeKal.HotelServices.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,12 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+	db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
