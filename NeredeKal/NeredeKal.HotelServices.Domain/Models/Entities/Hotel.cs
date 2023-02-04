e6e8d4a13e5296a6b925599883bcd12fc843b56b
Mustafa YAŞAR <mustafa@mustafayasar.info>
2023-02-04 20:48:46 +0300

    add entites

---
+++ NeredeKal/NeredeKal.HotelServices.Domain/Models/Entities/Hotel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeredeKal.HotelServices.Domain.Models.Entities
+{
+	public class Hotel:EntityBase<Guid>
+	{
+		public string AuthorizedName { get; set; }
+		public string AuthorizedLastName { get; set; }
+		public string CompanyName { get; set; }
+
+	}
+}
---
+++ NeredeKal/NeredeKal.HotelServices.Domain/Models/Enums/ContactType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeredeKal.HotelServices.Domain.Models.Enums
+{
+	public enum ContactType
+	{
+		PhoneNumber = 1,
+		EmailAddress,
+		Location
+	}
+}
